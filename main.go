328460f1852a17dfa806c06efd19ff48915d1bae
mAmineChniti <emin.chniti@esprit.tn>
2024-02-26 13:21:44 +0100

    started using echo package for clearer routing

--- main.go
+++ main.go
@@ -1,28 +1,32 @@
 package main
 
 import (
-	"fmt"
 	"html/template"
 	"log"
 	"net/http"
 	"os"
 	"path/filepath"
+	"strings"
+
+	"github.com/labstack/echo/v4"
 )
 
 func main() {
 	pagesDir := "pages/"
 	componentsDir := "components/"
-	port := 3500
+	port := ":3500"
 
 	logger := log.New(os.Stdout, "HTTP Server: ", log.LstdFlags)
 
-	http.HandleFunc("/status", func(w http.ResponseWriter, r *http.Request) {
+	e := echo.New()
+
+	e.GET("/status", func(c echo.Context) error {
 		logger.Printf("Working Fine!\n")
-		w.WriteHeader(http.StatusOK)
+		return c.String(http.StatusOK, "Working Fine!")
 	})
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		urlPath := r.URL.Path
+	e.GET("/", func(c echo.Context) error {
+		urlPath := c.Request().URL.Path
 		logger.Printf("Request: %s\n", urlPath)
 
 		if urlPath == "/" {
@@ -33,32 +37,30 @@ func main() {
 		_, err := os.Stat(filePath)
 		if err != nil {
 			logger.Printf("File not found: %s\n", filePath)
-			http.NotFound(w, r)
-			return
+			return c.String(http.StatusNotFound, "File not found")
 		}
 
 		logger.Printf("Serving file: %s\n", filePath)
 
 		tmpl := template.Must(template.ParseFiles(filePath))
-		tmpl.Execute(w, nil)
+		return tmpl.Execute(c.Response(), nil)
 	})
 
-	http.HandleFunc("/api/Box", func(w http.ResponseWriter, r *http.Request) {
-		componentName := r.URL.Path[len("/api/"):]
+	e.GET("/api/:Componet", func(c echo.Context) error {
+		componentName := strings.ToLower(c.Param("Componet"))
 		filePath := filepath.Join(componentsDir, componentName+".html")
 		_, err := os.Stat(filePath)
 		if err != nil {
 			logger.Printf("API component file not found: %s\n", filePath)
-			http.NotFound(w, r)
-			return
+			return c.String(http.StatusNotFound, "API component file not found")
 		}
 
 		logger.Printf("Serving API component: %s\n", filePath)
 
 		tmpl := template.Must(template.ParseFiles(filePath))
-		tmpl.Execute(w, nil)
+		return tmpl.Execute(c.Response(), nil)
 	})
 
-	logger.Printf("Server started on port %d\n", port)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), nil))
+	logger.Printf("Server started on port %s\n", port)
+	e.Logger.Fatal(e.Start(port))
 }
