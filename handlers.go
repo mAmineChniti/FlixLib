916802d2aeebd37d13fff6359a194fbe6ce5eb51
mAmineChniti <emin.chniti@esprit.tn>
2024-02-28 00:47:27 +0100

    separated functions into handlers added formatted middleware, and static components route

---
+++ handlers.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/labstack/echo/v4"
+)
+
+const (
+	pagesDir      = "pages/"
+	componentsDir = "components/"
+)
+
+func statusCheck(c echo.Context) error {
+	logger.Println("Working Fine!")
+	return c.String(http.StatusOK, "Working Fine!\n")
+}
+
+func pageServer(c echo.Context) error {
+	urlPath := c.Request().URL.Path
+	logger.Printf("Request: %s\n", urlPath)
+
+	if urlPath == "/" {
+		urlPath = "/index"
+	}
+
+	filePath := filepath.Join(pagesDir, urlPath+".html")
+	if _, err := os.Stat(filePath); err != nil {
+		logger.Printf("File not found: %s\n", filePath)
+		return c.String(http.StatusNotFound, "File not found\n")
+	}
+
+	logger.Printf("Serving file: %s\n", filePath)
+
+	tmpl := template.Must(template.ParseFiles(filePath))
+	return tmpl.Execute(c.Response(), nil)
+}
+
+func componentServer(c echo.Context, data any) error {
+	componentName := strings.ToLower(c.Param("Component"))
+	filePath := filepath.Join(componentsDir, componentName+".html")
+	if _, err := os.Stat(filePath); err != nil {
+		logger.Printf("API component file not found: %s\n", filePath)
+		return c.String(http.StatusNotFound, "API component file not found\n")
+	}
+
+	logger.Printf("Serving API component: %s\n", filePath)
+
+	tmpl := template.Must(template.ParseFiles(filePath))
+	return tmpl.Execute(c.Response(), data)
+}
