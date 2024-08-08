9106f5f983a7035194a74e8683e33c08644f1e4a
mAmineChniti <emin.chniti@esprit.tn>
2024-08-08 23:25:26 +0100

    graceful shutdown handling

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/mAmineChniti/FlixLib/pages"
@@ -8,17 +9,22 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func main() {
 	// Echo instance
 	app := echo.New()
+
 	// Middleware & Logger
 	var logger = log.New(os.Stdout, "HTTP Server: ", log.LstdFlags)
 	app.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
 		Format: "Request: Method=${method}, Uri=${uri}, Status=${status}\n",
 	}))
 	app.Use(middleware.CORS())
+
 	// Routes
 	app.GET("/status", func(c echo.Context) error {
 		return c.String(http.StatusOK, "OK")
@@ -30,11 +36,31 @@ func main() {
 		componentName := c.Param("componentName")
 		return utils.RenderComponent(c, componentName)
 	})
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
-	// Start server
-	logger.Println("Starting server on port " + port)
-	app.Logger.Fatal(app.Start(":" + port))
+
+	// Start server in a goroutine
+	go func() {
+		logger.Println("Starting server on port " + port)
+		if err := app.Start(":" + port); err != nil && err != http.ErrServerClosed {
+			app.Logger.Fatal("Shutting down the server")
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt, syscall.SIGTERM)
+	<-quit
+	logger.Println("Shutting down server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := app.Shutdown(ctx); err != nil {
+		app.Logger.Fatal("Server forced to shutdown:", err)
+	}
+
+	logger.Println("Server exiting")
 }
